67120e8cd81d259aef492cb291b42a56468820c8
dbyyyds659 <2065611673@qq.com>
2024-12-05 17:24:35 +0800

    Initial commit

---
+++ src/components/ToDoForm.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="stack-small">
+    <div class="custom-checkbox">
+      <input
+        type="checkbox"
+        class="checkbox"
+        :id="id"
+        :checked="isDone"
+        @change="$emit('checkbox-changed')" />
+      <label :for="id" class="checkbox-label">{{label}}</label>
+    </div>
+    <div class="btn-group">
+      <button type="button" class="btn" @click="toggleToItemEditForm">
+        Edit <span class="visually-hidden">{{label}}</span>
+      </button>
+      <button type="button" class="btn btn__danger" @click="deleteToDo">
+        Delete <span class="visually-hidden">{{label}}</span>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    onSubmit() {
+  if(this.label === "") {
+    return;
+  }
+  this.$emit('todo-added', this.label);
+  this.label = "";
+}
+
+
+
+
+},
+
+data() {
+  return {
+    isDone: this.done,
+    isEditing: false
+  };
+}
+
+};
+
+
+</script>
